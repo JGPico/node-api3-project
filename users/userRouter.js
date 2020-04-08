6ec2a253a88cca487bab1cd9a0f6b033146fe18b
JGPico <minq_2@hotmail.com>
2020-04-08 14:55:52 -0600

    Half-way

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
+const Users = require('./userDb.js');
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  Users.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    res.status(500).json({error: "Error adding user"});
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,11 +18,25 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+  .then(userArray => {
+    res.status(200).json(userArray);
+  })
+  .catch(err => {
+    res.status(500).json({error: "Failed to get users"});
+  });
+
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  console.log("req stuff", req.params.id);
+  Users.getById(req.params.id)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(err => {
+    res.status(500).json({error: "Failed to get user"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -33,11 +54,29 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  Users.getById(req.params.id)
+  .then(userBit => {
+    if (userBit) {
+      req.user = userBit;
+      next();
+    } else {
+      res.status(400).json({error: "invalid user id"});
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error: "Error fetching user"});
+  })
+  
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({error: "Missing user data"});
+  } else if (!req.body.name) {
+    res.status(400).json({error: "Missing required name field"});
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
